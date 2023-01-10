eaed017eebf272570f31cbec06b74734d21d267f
Marko Goronjic <marko.goronjic@quantox.com>
2023-01-10 22:03:22 +0100

    Click Events

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,18 @@
 
 export const Home = () => {
+    const handleClick = (event) => {
+        console.log('hello, ninjas', event.target)
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log('hello ' + name + ' ' + e.target)
+    }
+
     return (
         <div>
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button>
         </div>
     );
 };
