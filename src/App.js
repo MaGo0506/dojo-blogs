7e549468b870488d0182fcc4f134b0b17ac336b3
Marko Goronjic <marko.goronjic@quantox.com>
2023-01-10 21:50:08 +0100

    Adding Styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import './App.css';
 import Navbar from "./components/Navbar";
 import {Home} from "./components/Home";
 
 function App() {
     return (
         <div className="App">
-            <Navbar />
+            <Navbar/>
             <div className="content">
-               <Home/>
+                <Home/>
             </div>
         </div>
     );
