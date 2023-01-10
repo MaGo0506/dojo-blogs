a33662c37b74537c295ba968febe15a447ccd164
Marko Goronjic <marko.goronjic@quantox.com>
2023-01-10 21:42:03 +0100

    Multiple components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import './App.css';
+import Navbar from "./components/Navbar";
+import {Home} from "./components/Home";
 
 function App() {
-    const title = 'Welcome to the new blog';
-    const likes = 50;
-    // const person = {name: 'yoshi', age: 30}
-    const link = "https://www.google.com"
-
     return (
         <div className="App">
+            <Navbar />
             <div className="content">
-                <h1>{title}</h1>
-                <p>Liked {likes} times</p>
-                {/*{person}*/}
-
-                <p>{ "Hello Ninjas" }</p>
-                <p>{ [1,2,3,4,5] }</p>
-                <p>{ Math.random() * 10 }</p>
-
-                <a href={link}>Google Site</a>
+               <Home/>
             </div>
         </div>
     );
