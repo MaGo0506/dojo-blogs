9f189b94b3a9b43c0378a89ba6c92b45bbdc9992
Marko Goronjic <marko.goronjic@quantox.com>
2023-01-11 11:36:51 +0100

    Handling Fetch Errors

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,25 +4,35 @@ import {BlogList} from "./BlogList";
 export const Home = () => {
 
     const [blogs, setBlogs] = useState(null),
-        [isPending, setIsPending] = useState(true);
+        [isPending, setIsPending] = useState(true),
+        [error, setError] = useState(null)
 
     useEffect(() => {
         fetch('http://localhost:5000/blogs')
             .then(res => {
+                if (!res.ok) {
+                    throw Error('Could not fetch the data for that resource')
+                }
                 return res.json()
             })
             .then((data) => {
                 setBlogs(data)
                 setIsPending(false)
+                setError(null)
+            })
+            .catch(err => {
+                setIsPending(false)
+                setError(err.message);
             })
     }, []);
 
     return (
         <div className="home">
-            { isPending && <div>Loading...</div>}
-            {blogs && <BlogList
-                blogs={blogs}
-                title="All Blogs"/>}
+            { error && <div>{ error }</div> }
+            { isPending && <div>Loading...</div> }
+            { blogs && <BlogList
+                blogs={ blogs }
+                title="All Blogs"/> }
         </div>
     );
 };
