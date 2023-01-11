2632558092c7dae0c924a6628bfe2456026da913
Marko Goronjic <marko.goronjic@quantox.com>
2023-01-11 11:28:35 +0100

    Conditional Loading Message

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,8 @@ import {BlogList} from "./BlogList";
 
 export const Home = () => {
 
-    const [blogs, setBlogs] = useState(null);
+    const [blogs, setBlogs] = useState(null),
+        [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
         fetch('http://localhost:5000/blogs')
@@ -12,15 +13,16 @@ export const Home = () => {
             })
             .then((data) => {
                 setBlogs(data)
+                setIsPending(false)
             })
     }, []);
 
     return (
         <div className="home">
+            { isPending && <div>Loading...</div>}
             {blogs && <BlogList
                 blogs={blogs}
-                title="All Blogs"
-            />}
+                title="All Blogs"/>}
         </div>
     );
 };
