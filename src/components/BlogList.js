98882750e867d1809e870c81425229fa8cb6e0ec
Marko Goronjic <marko.goronjic@quantox.com>
2023-01-11 09:58:42 +0100

    Props

---
+++ src/components/BlogList.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+export const BlogList = (props) => {
+    const blogs = props.blogs,
+        title = props.title;
+
+    return (
+        <div className="blog-list">
+            <h1>{title}</h1>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
 import {useState} from "react";
+import {BlogList} from "./BlogList";
 
 export const Home = () => {
+
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
@@ -9,13 +11,10 @@ export const Home = () => {
 
     return (
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>{blog.body}</p>
-                    <p>Written by {blog.author}</p>
-                </div>
-            ))}
+            <BlogList
+                blogs={blogs}
+                title="All Blogs"
+            />
         </div>
     );
 };
