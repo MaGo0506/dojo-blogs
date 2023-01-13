3c22bfd410f20db0de5d89b880496ac71077805b
Marko Goronjic <marko.goronjic@quantox.com>
2023-01-13 22:16:28 +0100

    useEffect Cleanup

--- src/useFetch.js
+++ src/useFetch.js
@@ -7,23 +7,33 @@ export const useFetch = (url) => {
 
 
     useEffect(() => {
-        fetch(url)
-            .then(res => {
-                if (!res.ok) {
-                    throw Error('Could not fetch the data for that resource')
-                }
-                return res.json()
-            })
-            .then((data) => {
-                setData(data)
-                setIsPending(false)
-                setError(null)
-            })
-            .catch(err => {
-                setIsPending(false)
-                setError(err.message);
-            })
-    }, [url]);
+        const abortCont = new AbortController();
+
+        setTimeout(() => {
+            fetch(url, {signal: abortCont.signal})
+                .then(res => {
+                    if (!res.ok) {
+                        throw Error('Could not fetch the data for that resource')
+                    }
+                    return res.json()
+                })
+                .then((data) => {
+                    setData(data)
+                    setIsPending(false)
+                    setError(null)
+                })
+                .catch(err => {
+                    if (err.name === 'AbortError') {
+                        console.log('fetch aborted')
+                    } else {
+                        setIsPending(false)
+                        setError(err.message);
+                    }
+                })
+        }, 1000);
+
+        return () => abortCont.abort();
+    }, [url])
 
     return {
         data,
