18261a009b71b44865df8dbcc1b07f865085e922
Marko Goronjic <marko.goronjic@quantox.com>
2023-01-14 17:26:48 +0100

    Deleting Blogs

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,9 +1,19 @@
-import {useParams} from "react-router-dom";
+import {useHistory, useParams} from "react-router-dom";
 import {useFetch} from "../useFetch";
 
 export const BlogDetails = () => {
     const {id} = useParams()
+    const history = useHistory()
     const {data: blog, isPending, error} = useFetch(`http://localhost:5000/blogs/${id}`)
+
+    const handleDelete = () => {
+        fetch(`http://localhost:5000/blogs/${id}`, {
+            method: 'DELETE',
+        })
+            .then(() => {
+                history.push('/')
+            })
+    }
 
     return (
         <div className="blog-details">
@@ -14,7 +24,7 @@ export const BlogDetails = () => {
                     <h2>{blog.title}</h2>
                     <h4>Written by: {blog.author}</h4>
                     <p>{blog.body}</p>
-
+                    <button onClick={handleDelete}>Delete blog</button>
                 </article>
             )}
         </div>
