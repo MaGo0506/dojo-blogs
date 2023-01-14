4ffd7037e584781bf374cb00c8c22cc7789023cf
Marko Goronjic <marko.goronjic@quantox.com>
2023-01-14 14:32:00 +0100

    Reusing Custom Hooks

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,11 +1,22 @@
 import {useParams} from "react-router-dom";
+import {useFetch} from "../useFetch";
 
 export const BlogDetails = () => {
-    const { id } = useParams()
+    const {id} = useParams()
+    const {data: blog, isPending, error} = useFetch(`http://localhost:5000/blogs/${id}`)
 
     return (
         <div className="blog-details">
-            <h2>Blog details - { id }</h2>
+            {isPending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            {blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <h4>Written by: {blog.author}</h4>
+                    <p>{blog.body}</p>
+
+                </article>
+            )}
         </div>
     );
 };
